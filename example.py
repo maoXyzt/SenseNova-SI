94a5e4fa0c787fe9b3630866fa8e5a987561550a
yl-1993 <yangleidev@gmail.com>
2025-11-09 10:58:59 +0800

    feat: add multi-image MCQ example

--- example.py
+++ example.py
@@ -6,17 +6,27 @@ from transformers import AutoModel, AutoTokenizer
 from utils import load_image, split_model
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Example for SenseSI inference")
+    parser = argparse.ArgumentParser(description="Examples for SenseSI single-run MCQ")
     parser.add_argument(
         "--model_path",
         type=str,
         default="sensenova/SenseSI-InternVL3-8B",
         help="Model path",
     )
+    parser.add_argument(
+        "--image_paths",
+        type=str,
+        nargs="+",
+        default=["./examples/Q1_1.png"],
+        help="Path to image files, can specify multiple",
+    )
+    parser.add_argument(
+        "--question",
+        type=str,
+        default="<image>\nPlease describe the image in detail.",
+        help="Question to ask the model",
+    )
     args = parser.parse_args()
-
-    image_path = "./examples/Q1_1.png"
-    question = "<image>\nWhich figure is a top-down view of the given shape?\nOptions:\nA: A\nB: B\nC: C\nD: D"
 
     model_path = args.model_path
 
@@ -34,10 +44,25 @@ if __name__ == "__main__":
         model_path, trust_remote_code=True, use_fast=False
     )
 
-    pixel_values = load_image(image_path, max_num=12).to(torch.bfloat16).cuda()
+    pixel_values_list = []
+    print(f"Load {len(args.image_paths)} images...")
+    for path in args.image_paths:
+        print(f"Load image {path}...")
+        pixel_values_list.append(load_image(path, max_num=12).to(torch.bfloat16).cuda())
+
+    if len(pixel_values_list) > 1:
+        pixel_values = torch.cat(pixel_values_list, dim=0)
+    else:
+        pixel_values = pixel_values_list[0]
+
+    question = args.question
+
     generation_config = dict(
         do_sample=False, max_new_tokens=512, top_p=None, num_beams=1
     )
 
-    response = model.chat(tokenizer, pixel_values, question, generation_config)
-    print(f"User: {question}\nAssistant: {response}")
+    response = model.chat(
+        tokenizer, pixel_values, question, generation_config, history=None
+    )
+    print(f"User: {question}")
+    print(f"Assistant: {response}")
